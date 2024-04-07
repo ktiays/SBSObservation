bee5b2ebba8480469a250156a5774df912608dfe
Simon Støvring <mail@simonbs.dk>
2024-04-07 16:43:39 +0200

    Makes buffer init private

--- Sources/RunestoneObservation/Internal/ManagedCriticalState.swift
+++ Sources/RunestoneObservation/Internal/ManagedCriticalState.swift
@@ -6,10 +6,6 @@ struct ManagedCriticalState<State> {
     private let lock = NSLock()
     private let buffer: ManagedBuffer<State, UnsafeRawPointer>
 
-    init(_ buffer: ManagedBuffer<State, UnsafeRawPointer>) {
-        self.buffer = buffer
-    }
-
     init(_ initial: State) {
         let roundedSize = (MemoryLayout<UnsafeRawPointer>.size - 1) / MemoryLayout<UnsafeRawPointer>.size
         let buffer = LockedBuffer.create(minimumCapacity: Swift.max(roundedSize, 1)) { buffer in
@@ -18,7 +14,11 @@ struct ManagedCriticalState<State> {
         self.init(buffer)
     }
 
-    internal func withCriticalRegion<R>(_ critical: (inout State) throws -> R) rethrows -> R {
+    private init(_ buffer: ManagedBuffer<State, UnsafeRawPointer>) {
+        self.buffer = buffer
+    }
+
+    func withCriticalRegion<R>(_ critical: (inout State) throws -> R) rethrows -> R {
         try buffer.withUnsafeMutablePointers { header, lock in
             self.lock.lock()
             defer {
@@ -26,11 +26,5 @@ struct ManagedCriticalState<State> {
             }
             return try critical(&header.pointee)
         }
-    }
-}
-
-extension ManagedCriticalState: Identifiable {
-    var id: ObjectIdentifier {
-        ObjectIdentifier(buffer)
     }
 }
